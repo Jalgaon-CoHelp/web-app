f9526ce53345284b0ed556f297f6d21a4f93c17e
Shreyas <shreyaspatilg@gmail.com>
2021-05-09 10:16:17 +0530

    Add some TODO links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Container, Figure } from "react-bootstrap";
+import { Col, Container, Figure, Row } from "react-bootstrap";
 
 const Layout: React.FC = ({ children }) => {
   return (
@@ -20,7 +20,22 @@ const Layout: React.FC = ({ children }) => {
         </div>
       
       </Container>
-      <Container fluid className="border-top">{children}</Container>
+      <Container fluid className="border">{children}</Container>
+      <Container fluid className="pt-3 pl-3 pr-3">
+        <h3 className="m-3">Want to Contribute to Jalgaon CoHelp?</h3>
+        <Row className="border p-3">
+          <Col>You can help us by being volunteer with us and providing the valid information you have.</Col>
+          <Col> Show button here for registration</Col>
+        </Row>
+        <Row  className="border p-3">
+          <Col>Any hospital information is missing of invalid? Fill this form to help us.</Col>
+          <Col> Show button here for Google form link</Col>
+        </Row>
+        <Row  className="border p-3">
+          <Col>Do you have any feedback / grievance for Jalgaon CoHelp?</Col>
+          <Col> Show button here for Google form link</Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
