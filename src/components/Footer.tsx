318ecc5a23372750214c8a4848fdccb3b5517d36
Piyush Rajput <55524224+imPiyushrRajput@users.noreply.github.com>
2021-05-09 16:01:14 +0530

    PI | Done Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <Container fluid className="footer">
-      <Row>
-        <Col lg={12} md={12} sm={12} xs={12}>
-          Footer
+      <Row className="d-flex justify-content-center align-items-center">
+        <Col lg={6} md={6} sm={12} xs={12}>
+          <Link to="/privacy-policy">Privacy Policy</Link>
+        </Col>
+        <Col lg={6} md={6} sm={12} xs={12}>
+          <p>
+            This is an Open Source Project 
+            <a
+              href="https://github.com/jalgaon-cohelp"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Click here for the Code
+            </a>
+            Please contribute to make it better.
+          </p>
         </Col>
       </Row>
     </Container>
