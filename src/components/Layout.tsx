5ee97e3ce1be12d1f646baf30cc91a1f521292ef
Shreyas <shreyaspatilg@gmail.com>
2021-05-09 09:29:26 +0530

    Add header content for layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Container, Figure } from "react-bootstrap";
 
-const Layout:React.FC = ({ children }) => {
-    return (
-      <>
-        <Navbar />
-        <Container fluid>{children}</Container>
-        <Footer />
-      </>
-    );
-}
+const Layout: React.FC = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <Container className="p-3">
+        <div className="d-block mx-auto text-center">
+        <Figure>
+          <p className="font-weight-light">
+            The Crowdsourced resources to fight COVID across Jalgaon District. Find hospital beds, oxygen, plasma, and more.
+          </p>
+          <figcaption className="blockquote-footer">
+            From the Citizens, To the Citizens
+          </figcaption>
+        </Figure>
+        </div>
+      
+      </Container>
+      <Container fluid className="border-top">{children}</Container>
+      <Footer />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
