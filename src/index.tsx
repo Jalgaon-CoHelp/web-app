cf6a0c0c1bc63338a7b93b0720fd62dbdd63dc32
Ganesh Lohar <38778334+ganeshlohar59@users.noreply.github.com>
2021-05-13 00:04:49 +0530

    :art: Added app.styles.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles/app.css";
+import "./styles/app.styles.scss";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
